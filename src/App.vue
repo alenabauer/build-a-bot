c1737a0df4c4e0e7a313f07f4b1d7819799c6dca
Alena Bauer <alena.a.bauer@gmail.com>
2022-01-11 13:24:41 +0100

    add a simple navbar

--- src/App.vue
+++ src/App.vue
@@ -1,4 +1,14 @@
 <template>
+  <header>
+    <nav>
+      <ul>
+        <li class="nav-item">
+          <img class="logo" src="./assets/build-a-bot-logo.png" alt="logo">
+          Build-a-Bot
+        </li>
+      </ul>
+    </nav>
+  </header>
   <main>
     <HomePage />
   </main>
@@ -17,7 +27,7 @@ export default {
 
 <style>
 body {
-  background: linear-gradient(135deg, hsla(0, 0%, 100%, 1) 0%, hsla(0, 0%, 34%, 1) 100%);
+  background: hsla(0, 0%, 20%, 1);
   background-attachment: fixed;
 }
 #app {
@@ -26,7 +36,26 @@ body {
 main {
   margin: 0 auto;
   background-color: white;
-  width: 60%;
+  width: 1024px;
   padding: 32px;
+}
+header {
+  background-color: #999;
+  width: 1084px;
+  margin: 0 auto;
+}
+ul {
+  padding: 3px;
+  display: flex;
+}
+.nav-item {
+  display: inline-block;
+  padding: 5px 10px;
+  font-size: 22px;
+  border-right: 1px solid #bbb;
+}
+.logo {
+  vertical-align: middle;
+  height: 30px;
 }
 </style>
